12b9db5b3c650f9958ab2b9a4c11328458e588de
jaslieb <jas.lieb@hotmail.com>
2021-02-05 13:27:23 +0100

    Update README.md and add dump sql

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # docker-ynov
 A project developped during Docker classes at Ynov Campus Toulouse
+
+## Initiation
+
+- Install Docker 
+- Start nginx container with port 80
+- Serve a static .html file
+- [Optionaly work] Serve a static second .html file
+
+### Elements added 
+
+- `nginx.conf` nginx configuration
+- `www/` static folder
+
+## 1st TP
+
+- Start postgres container with port 5432
+- Start second postgres container with port 5433
+- Create a table into the second postgres database and dump the db
+
+- [Optionaly work] Encapsulate previous containers into a docker-compose .yml file
+
+### Elements added 
+
+- `dump.sql` second postgres database generated file
+- [Optionaly work] `docker-compose.yml` docker compose file
+- [Optionaly work] `db.env` postgres databases environnement files
---
+++ dump.sql
@@ -0,0 +1,81 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+
+--
+-- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(100)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, name) FROM stdin;
+0	TOTO
+\.
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
